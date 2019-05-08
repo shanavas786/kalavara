eb899b1761fea6555605acef099ecc93eec9859d
Shanavas M <shanavas.m2@gmail.com>
2019-05-08 16:19:46 +0530

    Remove query params from url path

--- src/master.rs
+++ src/master.rs
@@ -9,23 +9,42 @@ use std::str::FromStr;
 use std::sync::{Arc, RwLock};
 use std::thread;
 
+const STORE_PREFIX: &str = "/store/";
+const ADMIN_PREFIX: &str = "/admin/";
+
+/// returns the key from url string by removing /store/ prefix and query params if any
+fn get_key(url: &str) -> String {
+    let pfx_len = STORE_PREFIX.len();
+
+    // remove query params if any
+    match url.find('?') {
+        None => String::from(&url[pfx_len..]),
+        Some(indx) => String::from(&url[pfx_len..indx]),
+    }
+}
+
+#[test]
+fn test_get_key() {
+    let url = "/store/originalkey?q=this&that=that#foo";
+    assert_eq!(get_key(url), String::from("originalkey"));
+}
+
 fn admin_handle(_volumes: &RwLock<HashSet<String>>, req: Request) {
     // FIXME
     let _ = req.respond(resp!("admin"));
 }
 
 fn store_handler(db: &DB, volumes: &RwLock<HashSet<String>>, mut req: Request) {
-    // TODO remove query params
-    let path = String::from(req.url());
+    let key = get_key(req.url());
 
     let mut body = String::new();
     let _ = req.as_reader().read_to_string(&mut body);
 
     let _ = match *req.method() {
-        Method::Get => match db.get(path.as_bytes()) {
+        Method::Get => match db.get(key.as_bytes()) {
             Ok(Some(volume)) => {
                 let volume_url = volume.to_utf8().unwrap();
-                req.respond(redirect!(&format!("Location:{}{}", volume_url, path)))
+                req.respond(redirect!(&format!("Location:{}/{}", volume_url, key)))
             }
             Ok(None) => req.respond(resp!("Key not found", 404)),
             Err(_) => req.respond(resp!("Server Error", 500)),
@@ -42,23 +61,22 @@ fn store_handler(db: &DB, volumes: &RwLock<HashSet<String>>, mut req: Request) {
                     volume = vlms.iter().choose(&mut rng);
                 }
 
-                match db.put(path.as_bytes(), volume.unwrap()) {
+                match db.put(key.as_bytes(), volume.unwrap()) {
                     Ok(_) => {
-                        req.respond(redirect!(&format!("Location:{}{}", volume.unwrap(), path)))
+                        req.respond(redirect!(&format!("Location:{}/{}", volume.unwrap(), key)))
                     }
                     Err(_) => req.respond(resp!("Server Error", 500)),
                 }
             }
         }
         Method::Delete => {
-            match db.get(path.as_bytes()) {
+            match db.get(key.as_bytes()) {
                 Ok(Some(volume)) => {
                     let volume_url = volume.to_utf8().unwrap();
-                    println!("key found on volume {}", volume_url);
 
                     // delete it from db
-                    db.delete(path.as_bytes()).unwrap();
-                    req.respond(redirect!(&format!("Location:{}{}", volume_url, path)))
+                    db.delete(key.as_bytes()).unwrap();
+                    req.respond(redirect!(&format!("Location:{}/{}", volume_url, key)))
                 }
                 Ok(None) => req.respond(resp!("Key not found", 404)),
                 Err(_) => req.respond(resp!("Server Error", 500)),
@@ -71,9 +89,9 @@ fn store_handler(db: &DB, volumes: &RwLock<HashSet<String>>, mut req: Request) {
 fn req_handler(db: &DB, volumes: &RwLock<HashSet<String>>, req: Request) {
     let path = String::from(req.url());
 
-    if path.starts_with("/store/") {
+    if path.starts_with(STORE_PREFIX) {
         store_handler(db, volumes, req);
-    } else if path.starts_with("/admin/") {
+    } else if path.starts_with(ADMIN_PREFIX) {
         admin_handle(volumes, req);
     } else {
         let _ = req.respond(resp!("Invalid Path", 404));
