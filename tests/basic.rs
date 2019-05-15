fcf5c51c43fcadc37e4effc91584eb4816cfaaae
Shanavas M <shanavas.m2@gmail.com>
2019-05-15 18:19:17 +0530

    Add test for removal of query params

--- tests/basic.rs
+++ tests/basic.rs
@@ -2,8 +2,11 @@ use kalavara::master::start as master_start;
 use kalavara::volume::start as volume_start;
 use reqwest::Client;
 
+use std::sync::{Once, ONCE_INIT};
 use std::thread;
 use std::time::Duration;
+
+static INIT: Once = ONCE_INIT;
 
 fn run() {
     thread::spawn(move || {
@@ -20,12 +23,19 @@ fn run() {
     });
 }
 
+/// Setup function that is only run once, even if called multiple times.
+/// to be called by all tests.
+fn setup() {
+    INIT.call_once(|| {
+        println!("here we go");
+        run();
+        thread::sleep(Duration::from_millis(1000));
+    });
+}
+
 #[test]
 fn test_kv() {
-    run();
-
-    // let ther servers ready
-    thread::sleep(Duration::from_millis(1000));
+    setup();
 
     let res = Client::new()
         .put("http://localhost:6000/store/key1")
@@ -48,6 +58,36 @@ fn test_kv() {
     assert_eq!(res.unwrap().status(), 204);
 
     let res = Client::new().get("http://localhost:6000/store/key1").send();
+    assert!(res.is_ok());
+    assert_eq!(res.unwrap().status(), 404);
+}
+
+#[test]
+fn test_remove_query_params() {
+    setup();
+    let res = Client::new()
+        .put("http://localhost:6000/store/key2?query=value")
+        .body("val2")
+        .send();
+
+    assert!(res.is_ok());
+    assert_eq!(res.unwrap().status(), 201);
+
+    let res = Client::new()
+        .get("http://localhost:6000/store/key2?que=valu")
+        .send();
+    assert!(res.is_ok());
+    let mut res = res.unwrap();
+    assert_eq!(res.status(), 200);
+    assert_eq!(res.text().unwrap(), "val2");
+
+    let res = Client::new()
+        .delete("http://localhost:6000/store/key2?q=v")
+        .send();
+    assert!(res.is_ok());
+    assert_eq!(res.unwrap().status(), 204);
+
+    let res = Client::new().get("http://localhost:6000/store/key2").send();
     assert!(res.is_ok());
     assert_eq!(res.unwrap().status(), 404);
 }
