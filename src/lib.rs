0ff7b67ffe7e0fa4c1f4d35acc877345ab7f025c
Shanavas M <shanavas.m2@gmail.com>
2019-05-24 12:24:35 +0530

    Refactor master server

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,64 @@
+use tiny_http::{Method, Request};
+
+use std::io::{empty as empty_reader, Read};
+
+/// returns the key from url string by removing /store/ prefix and query params if any
+fn get_key(url: &str, prefix: &str) -> String {
+    let pfx_len = prefix.len();
+
+    // remove query params if any
+    match url.find('?') {
+        None => String::from(&url[pfx_len..]),
+        Some(indx) => String::from(&url[pfx_len..indx]),
+    }
+}
+
+#[test]
+fn test_get_key() {
+    let url = "/store/originalkey?q=this&that=that#foo";
+    assert_eq!(get_key(url, "/store/"), String::from("originalkey"));
+}
+
+/// Trait that send http response to a request
+/// ResponseKind Should implement this
+trait Respond {
+    fn respond(self, req: Request);
+}
+
+/// Kalavara Store service trait
+/// Defines methods that stores needs to implement
+trait Service: Sync + Send {
+    /// ResponseTye
+    /// Should know how to respond to a request
+    type Response: Respond + Default;
+
+    /// Get url prefix
+    fn get_prefix(&self) -> &'static str;
+
+    /// Get a key from store
+    fn get(&self, key: String) -> Self::Response;
+
+    /// Save/Update key in store
+    fn save(&self, key: String, value: impl Read) -> Self::Response;
+
+    /// Remove a key from store
+    fn delete(&self, key: String) -> Self::Response;
+
+    /// Dispatch a request to respective handler methods
+    fn dispatch(&self, req: Request) {
+        let key = get_key(req.url(), self.get_prefix());
+
+        let resp = match *req.method() {
+            Method::Get => self.get(key),
+            Method::Post | Method::Put => self.save(key, Box::new(empty_reader())),
+            Method::Delete => self.delete(key),
+            _ => Default::default(),
+        };
+
+        resp.respond(req);
+    }
+}
+
 #[macro_use]
 mod macros;
 pub mod master;
--- src/master.rs
+++ src/master.rs
@@ -1,124 +1,167 @@
 use rand::seq::IteratorRandom;
 use rand::thread_rng;
 use rocksdb::DB;
-use tiny_http::{Method, Request};
 
 use std::collections::HashSet;
+use std::io::Read;
 use std::net::SocketAddr;
 use std::str::FromStr;
 use std::sync::{Arc, RwLock};
 use std::thread;
+use tiny_http::{Request, Server};
+
+use crate::{Respond, Service};
 
 const STORE_PREFIX: &str = "/store/";
-const ADMIN_PREFIX: &str = "/admin/";
 
-/// returns the key from url string by removing /store/ prefix and query params if any
-fn get_key(url: &str) -> String {
-    let pfx_len = STORE_PREFIX.len();
+/// Master store
+struct Master {
+    db: Arc<DB>,
+    volumes: Arc<RwLock<HashSet<String>>>,
+}
 
-    // remove query params if any
-    match url.find('?') {
-        None => String::from(&url[pfx_len..]),
-        Some(indx) => String::from(&url[pfx_len..indx]),
+/// Types of responses that master generates
+enum ResponseKind {
+    /// Redirect to volume server, 301
+    Redirect(String),
+
+    /// Key not found, 404
+    NotFound,
+
+    /// Error occured, 500
+    ServerError,
+
+    /// Method not allowed, 405
+    NotAllowed,
+
+    /// Unavailable, 503
+    Unavailable,
+}
+
+impl Default for ResponseKind {
+    fn default() -> Self {
+        ResponseKind::NotAllowed
     }
 }
 
-#[test]
-fn test_get_key() {
-    let url = "/store/originalkey?q=this&that=that#foo";
-    assert_eq!(get_key(url), String::from("originalkey"));
+impl Respond for ResponseKind {
+    fn respond(self, req: Request) {
+        use ResponseKind::*;
+
+        let _ = match self {
+            Redirect(url) => req.respond(redirect!(&format!("Location:{}", url))),
+            NotFound => req.respond(resp!("Key not found", 404)),
+            ServerError => req.respond(resp!("Server error", 500)),
+            NotAllowed => req.respond(resp!("Method not allowd", 405)),
+            Unavailable => req.respond(resp!("Service unavailable", 503)),
+        };
+    }
 }
 
-fn admin_handle(_volumes: &RwLock<HashSet<String>>, req: Request) {
-    // FIXME
-    let _ = req.respond(resp!("admin"));
+impl Master {
+    pub fn new(db: DB, volumes: Vec<String>) -> Master {
+        Master {
+            db: Arc::new(db),
+            volumes: Arc::new(RwLock::new(volumes.into_iter().collect())),
+        }
+    }
+
+    /// translate key to volume url
+    /// TODO: more intelligent selection
+    fn key_to_volume(&self, _key: &str) -> String {
+        let mut rng = thread_rng();
+        let mut volume = None;
+        let volumes = self.volumes.read().unwrap();
+
+        while volume.is_none() {
+            volume = volumes.iter().choose(&mut rng);
+        }
+
+        volume.unwrap().to_owned()
+    }
 }
 
-fn store_handler(db: &DB, volumes: &RwLock<HashSet<String>>, mut req: Request) {
-    let key = get_key(req.url());
+impl Service for Master {
+    type Response = ResponseKind;
 
-    let mut body = String::new();
-    let _ = req.as_reader().read_to_string(&mut body);
+    fn get_prefix(&self) -> &'static str {
+        STORE_PREFIX
+    }
 
-    let _ = match *req.method() {
-        Method::Get => match db.get(key.as_bytes()) {
+    fn get(&self, key: String) -> Self::Response {
+        match self.db.get(key.as_bytes()) {
+            Ok(Some(volume)) => {
+                ResponseKind::Redirect(format!("{}/{}", volume.to_utf8().unwrap().to_string(), key))
+            }
+            Ok(None) => ResponseKind::NotFound,
+            Err(_) => ResponseKind::ServerError,
+        }
+    }
+
+    fn save(&self, key: String, _value: impl Read) -> Self::Response {
+        let vlms = self.volumes.read().unwrap();
+
+        if vlms.is_empty() {
+            ResponseKind::Unavailable
+        } else {
+            let volume = self.key_to_volume(&key);
+            match self.db.put(key.as_bytes(), volume.as_bytes()) {
+                Ok(_) => ResponseKind::Redirect(format!("{}/{}", volume, key)),
+                Err(_) => ResponseKind::ServerError,
+            }
+        }
+    }
+
+    fn delete(&self, key: String) -> Self::Response {
+        match self.db.get(key.as_bytes()) {
             Ok(Some(volume)) => {
                 let volume_url = volume.to_utf8().unwrap();
-                req.respond(redirect!(&format!("Location:{}/{}", volume_url, key)))
-            }
-            Ok(None) => req.respond(resp!("Key not found", 404)),
-            Err(_) => req.respond(resp!("Server Error", 500)),
-        },
-        Method::Post | Method::Put => {
-            let vlms = volumes.read().unwrap();
 
-            if vlms.is_empty() {
-                req.respond(resp!("No volume servers found", 503))
-            } else {
-                let mut rng = thread_rng();
-                let mut volume = None;
-                while volume.is_none() {
-                    volume = vlms.iter().choose(&mut rng);
-                }
-
-                match db.put(key.as_bytes(), volume.unwrap()) {
-                    Ok(_) => {
-                        req.respond(redirect!(&format!("Location:{}/{}", volume.unwrap(), key)))
-                    }
-                    Err(_) => req.respond(resp!("Server Error", 500)),
+                // delete it from db
+                if self.db.delete(key.as_bytes()).is_ok() {
+                    ResponseKind::Redirect(format!("{}/{}", volume_url, key))
+                } else {
+                    ResponseKind::ServerError
                 }
             }
+            Ok(None) => ResponseKind::NotFound,
+            Err(_) => ResponseKind::ServerError,
         }
-        Method::Delete => {
-            match db.get(key.as_bytes()) {
-                Ok(Some(volume)) => {
-                    let volume_url = volume.to_utf8().unwrap();
-
-                    // delete it from db
-                    db.delete(key.as_bytes()).unwrap();
-                    req.respond(redirect!(&format!("Location:{}/{}", volume_url, key)))
-                }
-                Ok(None) => req.respond(resp!("Key not found", 404)),
-                Err(_) => req.respond(resp!("Server Error", 500)),
-            }
-        }
-        _ => req.respond(resp!("Method not allowed", 405)),
-    };
-}
-
-fn req_handler(db: &DB, volumes: &RwLock<HashSet<String>>, req: Request) {
-    let path = String::from(req.url());
-
-    if path.starts_with(STORE_PREFIX) {
-        store_handler(db, volumes, req);
-    } else if path.starts_with(ADMIN_PREFIX) {
-        admin_handle(volumes, req);
-    } else {
-        let _ = req.respond(resp!("Invalid Path", 404));
     }
 }
 
+/// starts a kalavara master server
+/// # Arguments
+///
+/// * `port` - Port name to listen at
+/// * `data_dir` - Database directory
+/// * `threads` - Number of threads to spawn
+/// * `volumes` - List of volume servers
+///
 pub fn start(port: u16, data_dir: &str, threads: u16, volumes: Vec<String>) {
     let db = match DB::open_default(data_dir) {
-        Ok(db) => Arc::new(db),
+        Ok(db) => db,
         Err(e) => panic!("failed to open database: {:?}", e),
     };
 
-    let volumes: Arc<RwLock<HashSet<String>>> =
-        Arc::new(RwLock::new(volumes.into_iter().collect()));
     let addr: SocketAddr = ([0, 0, 0, 0], port).into();
 
-    let server = Arc::new(tiny_http::Server::http(addr).unwrap());
+    let server = match Server::http(addr) {
+        Ok(server) => Arc::new(server),
+        Err(e) => panic!("failed to start http server: {:?}", e),
+    };
+
+    let master = Arc::new(Master::new(db, volumes));
+
     let mut handles = Vec::new();
 
     for _ in 0..threads {
         let server = server.clone();
-        let db = db.clone();
-        let volumes = volumes.clone();
+        let handler = master.clone();
 
         handles.push(thread::spawn(move || {
             for rq in server.incoming_requests() {
-                req_handler(&db, &volumes, rq);
+                handler.dispatch(rq);
             }
         }));
     }
